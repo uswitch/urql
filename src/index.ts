43da689c0e9301c06d6e5ff578989b377492a96f
Jovi De Croock <jovi.decroock@formidable.com>
2019-09-10 10:33:32 +0200

    (chore) - export utils from client

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,3 @@ export * from './client';
 export * from './context';
 export * from './components';
 export * from './hooks';
-
-export {
-  CombinedError,
-  createRequest,
-  makeResult,
-  makeErrorResult,
-  formatDocument,
-} from './utils';
